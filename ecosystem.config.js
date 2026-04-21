53bdaa307bc988a64a8560eee8a8a2cc6ed392a1
RjM091309 <rjmanapsal@gmail.com>
2026-04-21 09:53:27 +0800

    Update rename db

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         DB_HOST: "127.0.0.1",
         DB_USER: "3core",
         DB_PASSWORD: "2024.3core21",
-        DB_NAME: "GDCage",
+        DB_NAME: "gdcage",
         DB_PORT: 3306,
         REDIS_HOST: "127.0.0.1",
         REDIS_PORT: 6379,
